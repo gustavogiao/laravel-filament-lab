89f26bad043dab9ce13d5cba10e12b34772d8d23
Gustavo Gião <gustavogiao@ua.pt>
2026-01-28 18:30:30 +0000

    refactor: remove Fortify dependency and related service provider; clean up dashboard route and update test descriptions; add goToStep to wizard and fix notification animation callback

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,4 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Modules\User\Providers\UserServiceProvider::class,
-    App\Modules\Permissions\Providers\PermissionsServiceProvider::class,
-    App\Modules\Auth\Providers\FortifyServiceProvider::class,
-];
+    App\Modules\Permissions\Providers\PermissionsServiceProvider::class,];
